801bd77a05b07872dfd75450aaee8e4fdec5accc
KrisTinaS-ops <tina.u@mail.ru>
2021-06-08 22:29:25 +0300

    lesson_5

---
+++ Savina_Kristina_dz_5/task_5_2.py
@@ -0,0 +1,8 @@
+max_val = 15
+odd_nums = (i for i in range(1, max_val + 1,  2))
+
+print(type(odd_nums))
+
+print(next(odd_nums))
+print(next(odd_nums))
+print(next(odd_nums))
---
+++ Savina_Kristina_dz_5/task_5_3.py
@@ -0,0 +1,19 @@
+from itertools import zip_longest
+
+tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена']
+klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
+
+gen = (qwe for qwe in zip(tutors, klasses))
+print(next(gen))
+
+gen = ((tutor, klass) for tutor, klass in zip_longest(tutors, klasses))
+print((type(gen)))
+
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
